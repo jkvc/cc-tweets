e49a4b82e4f3c28c27fdd3db9ea3d85a69640db4
Junshen Kevin Chen <kevinehc@gmail.com>
2021-02-18 17:06:15 -0800

    describe.ip

---
+++ cc_tweets/04_describe_data.py
@@ -0,0 +1,47 @@
+import json
+import re
+import string
+from collections import Counter, OrderedDict
+from datetime import datetime
+from glob import glob
+from os.path import exists, join
+from pprint import pprint
+from typing import List
+
+import matplotlib.pyplot as plt
+from config import DATA_DIR, RAW_DIR
+from nltk.corpus import stopwords
+from nltk.stem.snowball import SnowballStemmer
+from tqdm import tqdm
+
+from cc_tweets.utils import ParallelHandler, load_pkl, mkdir_overwrite, save_pkl
+
+DATASET_NAME = "tweets_downsized100_filtered"
+PKL_PATH = join(DATA_DIR, f"{DATASET_NAME}.pkl")
+SAVE_DIR = join(DATA_DIR, f"describe_{DATASET_NAME}")
+
+# Fri Nov 30 19:41:04 +0000 2018
+TIME_FORMAT = "%a %b %d %H:%M:%S %z %Y"
+
+
+if __name__ == "__main__":
+    tweets = load_pkl(PKL_PATH)
+
+    mkdir_overwrite(SAVE_DIR)
+
+    # count stances
+    stances = dict(Counter(t["stance"] for t in tweets))
+    plt.bar(stances.keys(), stances.values())
+    plt.savefig(join(SAVE_DIR, "stances_count.png"))
+    plt.clf()
+
+    # count time
+    times = [datetime.strptime(tweet["time"], TIME_FORMAT) for tweet in tweets]
+    # aggregate by year
+    yyyymms = [f"{t.year}" for t in times]
+    agg_year = dict(Counter(yyyymms))
+    agg_year = OrderedDict(sorted(agg_year.items()))
+    pprint(agg_year)
+    plt.bar(agg_year.keys(), agg_year.values())
+    plt.savefig(join(SAVE_DIR, "agg_year.png"))
+    plt.clf()
