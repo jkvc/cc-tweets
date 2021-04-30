5f1c8d334273aea2932a254ffa242e2882dfbd0b
Junshen Kevin Chen <kevinehc@gmail.com>
2021-04-29 21:01:14 -0700

    logodds.engagement

--- cc_tweets/2.word_level/4.log_odds_engagement.py
+++ cc_tweets/2.word_level/4.log_odds_engagement.py
@@ -5,11 +5,13 @@ from os.path import join
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
+from cc_tweets.data_utils import get_ngrams
 from cc_tweets.experiment_config import SUBSET_PKL_PATH, SUBSET_WORKING_DIR
 from cc_tweets.log_odds import scaled_lor
 from cc_tweets.utils import load_pkl, mkdir_overwrite, unzip
 
 ENGAGEMENTS = ["retweets", "likes"]
+MIN_NUM_FOLLOWER = 30
 
 if __name__ == "__main__":
     savedir = join(SUBSET_WORKING_DIR, "log_odds_engagement")
@@ -22,9 +24,9 @@ if __name__ == "__main__":
 
         ratios = np.array(
             [
-                np.log(t[engagement_type] + 1) / np.log(t["max_num_follower"])
+                np.log(t[engagement_type] + 1) / np.log(t["max_num_follower"] + 1)
                 for t in tweets
-                if t["max_num_follower"] > 10
+                if t["max_num_follower"] >= MIN_NUM_FOLLOWER
             ]
         )
         plt.clf()
@@ -33,22 +35,27 @@ if __name__ == "__main__":
 
         median = np.median(ratios)
 
-        hi_engagement_wc, lo_engagement_wc = Counter(), Counter()
-        for t in tweets:
-            if t["max_num_follower"] <= 10:
-                continue
-            ratio = np.log(t[engagement_type] + 1) / np.log(t["max_num_follower"])
-            wc = hi_engagement_wc if ratio > median else lo_engagement_wc
-            wc.update(t["lemmas"])
+        for toktype, ngram in [("lemmas", 1), ("stems", 2)]:
+            hi_engagement_wc, lo_engagement_wc = Counter(), Counter()
+            for t in tweets:
+                if t["max_num_follower"] < MIN_NUM_FOLLOWER:
+                    continue
+                ratio = np.log(t[engagement_type] + 1) / np.log(
+                    t["max_num_follower"] + 1
+                )
+                wc = hi_engagement_wc if ratio > median else lo_engagement_wc
+                wc.update(get_ngrams(t[toktype], ngram))
 
-        lor0w = scaled_lor(hi_engagement_wc, lo_engagement_wc, {})
-        hi_engagement_topwords = [w for lor, w in lor0w][:100]
-        lo_engagement_topwords = [w for lor, w in lor0w[::-1]][:100]
-        print(hi_engagement_topwords)
-        print(lo_engagement_topwords)
+            lor0w = scaled_lor(hi_engagement_wc, lo_engagement_wc, {})
+            hi_engagement_topwords = [w for lor, w in lor0w][:100]
+            lo_engagement_topwords = [w for lor, w in lor0w[::-1]][:100]
+            print(hi_engagement_topwords)
+            print(lo_engagement_topwords)
 
-        scores, words = unzip(lor0w)
-        df = pd.DataFrame()
-        df["word"] = words
-        df["score"] = scores
-        df.to_csv(join(savedir, f"{engagement_type}.csv"), index=False)
+            scores, words = unzip(lor0w)
+            df = pd.DataFrame()
+            df["word"] = words
+            df["score"] = scores
+            df.to_csv(
+                join(savedir, f"{engagement_type}.{toktype}.{ngram}.csv"), index=False
+            )
